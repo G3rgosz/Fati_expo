a682ae3941f6781c32760f9f61c6b928ef96ccb5
Gergosz-2000 <gergoszivak@gmail.com>
2022-01-03 11:13:58 +0100

    mukodik

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { getBooks } from './shared/api';
 
 export default function App() {
+  const [books, setBooks] = useState();
+
+  useEffect( () => {
+    getBooks()
+    .then(res => {
+      setBooks(res)
+      console.log(res[0].title)
+    })
+  },[])
+  function onLoadButton(){
+    console.log("letöltés")
+    getBooks()
+    .then(res => {
+      setBooks(res)
+      console.log(res[0].title)
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.head}>Könyvek</Text>
+      <View style={styles.bookList}>
+        <FlatList 
+          data={books}
+          renderItem={ ({item}) => (
+            <Text style={styles.title}>{item.title}</Text>
+          )}
+        />
+      </View>
+      <Button 
+        title="letölt"
+        onPress={onLoadButton}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  head: {
+    fontSize: 32,
+    color: '#fff',
+  },
+  title: {
+    fontSize: 20,
+    padding: 15,
+    marginBottom: 15,
+    backgroundColor: "yellow",
+  },
+  bookList: {
+    width: '100%',
+    backgroundColor: "red",
+    marginVertical: 30,
+  }
 });
